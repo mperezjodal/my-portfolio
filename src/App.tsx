142561e904e7cc09a551621324575e0f79f4085f
Marcelo Perez <marceloperez@MacBook-Pro-2.local>
2022-03-18 10:44:00 -0300

    Title, linkedin and email links

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bottom from './Components/Bottom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Marcelo Pérez Jodal
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Bottom />
       </header>
     </div>
   );
---
+++ src/Components/Bottom/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function Bottom() {
+    return (
+        <p>
+            <a
+                className="App-link"
+                href="https://www.linkedin.com/in/marcelo-pérez-jodal-28553454/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Linkedin
+            </a>
+            {` - `}
+            <a
+                className="App-link"
+                href="mailto:mperezjodal@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Email
+            </a>
+        </p>
+    );
+}
+
+export default Bottom;
