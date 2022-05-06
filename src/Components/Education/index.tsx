bfd47ee509540b511bab7c296f75fd6afa14e1b3
Marcelo Perez <marceloperez@MacBook-Pro-2.local>
2022-05-06 13:49:14 -0300

    Bug fixes

--- src/Components/Education/index.tsx
+++ src/Components/Education/index.tsx
@@ -11,11 +11,11 @@ function SecondInfoComponent() {
                 <strong><a href="https://fi.ort.edu.uy/ingenieria-en-sistemas" target="_blank">Systems Engineering</a></strong> (<i>2019 through 2023</i>) - Universidad ORT Uruguay<br /> 
                 <br /> 
                 I'm currently finishing the 5 year graduate degree in Systems Engineering with a full scholarship awarded for the first place in the Uruguayan Mathematical Olympiad.
-                Grade average: 92%, scholarship  available <a href="https://drive.google.com/file/d/1MG8yrrOKGgWv7fRpl6lMQjim82-2A-l_/view?usp=sharing">here</a>.
+                Grade average: 92%, scholarship  available <a href="https://drive.google.com/file/d/1MG8yrrOKGgWv7fRpl6lMQjim82-2A-l_/view?usp=sharing" target="_blank">here</a>.
             </p>
             <br /> 
             <p>
-                <strong><a href="https://www.fhuce.edu.uy/index.php/ensenanza/licenciaturas/licenciatura-en-filosofia" target="_blank">BA in Philosophy</a></strong> (<i>2019 through 2020</i>) - Univerdidad de la República<br /> 
+                <strong><a href="https://www.fhuce.edu.uy/index.php/ensenanza/licenciaturas/licenciatura-en-filosofia" target="_blank">BA in Philosophy</a></strong> (<i>2019 through 2020</i>) - Universidad de la República<br /> 
                 <br /> 
                 Completed one third of this degree's courses. Scholarship <a href="https://drive.google.com/file/d/1fVrB1g8WBe0604VaLCB54IcJlB10dlm4/view?usp=sharing" target="_blank">here</a>.
             </p>
