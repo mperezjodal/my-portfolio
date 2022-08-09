3550e725b4feb2219613ac5b2eff4df01a0f3ea7
Marcelo Perez <marceloperez@MacBook-Pro-2.local>
2022-08-09 11:49:37 -0300

    Many updates

--- src/Components/Achievements/index.tsx
+++ src/Components/Achievements/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import '../../App.css';
 
-function AchievementsComponent() {
+function AchievementsComponent() {  
     return (
         <div className="AchievementsComponent">
             <p className="Title">
@@ -10,11 +10,16 @@ function AchievementsComponent() {
             <p>
                 <strong>First Place in Uruguay's Mathematical Olympiad</strong> (<i>2017 and 2018</i>)<br /> 
                 <br /> 
-                Second and third place in 2016 and 2015.
+                Second and third places in 2016 and 2015.
             </p>
             <br /> 
             <p>
                 <strong>3-time contestant in the <a href="https://www.imo-official.org" target="_blank">International Mathematical Olympiad</a> </strong> (<i>2017, 2018 and 2019</i>)
+                <br /> 
+            </p>
+            <br /> 
+            <p>
+                <strong>Full scholarship in ORT University  </strong>
                 <br /> 
             </p>
         </div>
--- src/Components/Education/index.tsx
+++ src/Components/Education/index.tsx
@@ -11,13 +11,13 @@ function SecondInfoComponent() {
                 <strong><a href="https://fi.ort.edu.uy/ingenieria-en-sistemas" target="_blank">Systems Engineering</a></strong> (<i>2019 through 2023</i>) - Universidad ORT Uruguay<br /> 
                 <br /> 
                 I'm currently finishing the 5 year graduate degree in Systems Engineering with a full scholarship awarded for the first place in the Uruguayan Mathematical Olympiad.
-                Grade average: 93% (4.0 GPA), scholarship  available <a href="https://drive.google.com/file/d/1MG8yrrOKGgWv7fRpl6lMQjim82-2A-l_/view?usp=sharing" target="_blank">here</a>.
+                Participated in programming competitions. Grade average: 93% (4.0 GPA), scholarship  available <a href="https://drive.google.com/file/d/1MG8yrrOKGgWv7fRpl6lMQjim82-2A-l_/view?usp=sharing" target="_blank">here</a>.
             </p>
             <br /> 
             <p>
                 <strong><a href="https://www.fhuce.edu.uy/index.php/ensenanza/licenciaturas/licenciatura-en-filosofia" target="_blank">BA in Philosophy</a></strong> (<i>2019 through 2020</i>) - Universidad de la República<br /> 
                 <br /> 
-                Completed one third of this degree's courses. Scholarship <a href="https://drive.google.com/file/d/1fVrB1g8WBe0604VaLCB54IcJlB10dlm4/view?usp=sharing" target="_blank">here</a>.
+                Completed 1/3 of this degree. Scholarship <a href="https://drive.google.com/file/d/1fVrB1g8WBe0604VaLCB54IcJlB10dlm4/view?usp=sharing" target="_blank">here</a>.
             </p>
         </div>
     );
